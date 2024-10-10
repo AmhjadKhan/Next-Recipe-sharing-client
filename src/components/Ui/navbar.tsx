1c8514064254ecb07ca6dde997b629b755574bdb
Amhjadkhan <amjadpogramer@gmail.com>
2024-10-11 01:54:05 +0400

    this is navber and layout commit

---
+++ src/components/Ui/navbar.tsx
@@ -0,0 +1,92 @@
+"use client";
+import {
+  Navbar as NextUINavbar,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuToggle,
+  NavbarBrand,
+  NavbarItem,
+  NavbarMenuItem,
+} from "@nextui-org/navbar";
+import { Link } from "@nextui-org/link";
+import { link as linkStyles } from "@nextui-org/theme";
+import NextLink from "next/link";
+import clsx from "clsx";
+import { Button } from "@nextui-org/button";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+import { siteConfig } from "@/src/config/site";
+import { ThemeSwitch } from "@/src/components/theme-switch";
+
+export const Navbar = () => {
+  const pathname = usePathname();
+
+  return (
+    <NextUINavbar className="py-4" maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <p className="font-bold text-inherit">🍔Recipet</p>
+          </NextLink>
+        </NavbarBrand>
+        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+          {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href} isActive={item.href === pathname}>
+              <NextLink
+                className={clsx(linkStyles({ color: "foreground" }))}
+                href={item.href}
+              >
+                {item.label}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </ul>
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem className="hidden sm:flex gap-2">
+          <ThemeSwitch />
+        </NavbarItem>
+
+        <NavbarItem className="hidden sm:flex gap-2">
+          <Link href="/login">
+            <Button color="danger" variant="shadow">
+              Login
+            </Button>
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+        <ThemeSwitch />
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+      <NavbarMenu className="bg-gradient-to-r from-customColor1/50 to-customColor2/50 dark:bg-gradient-to-r dark:from-default-50 dark:to-default-100">
+        <div className="mx-4 mt-8 flex flex-col gap-2">
+          {siteConfig.navMenuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                color={
+                  index === 2
+                    ? "primary"
+                    : index === siteConfig.navMenuItems.length - 1
+                    ? "danger"
+                    : "foreground"
+                }
+                href={item.href}
+                size="lg"
+              >
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
+    </NextUINavbar>
+  );
+};
